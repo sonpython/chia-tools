a7c9d0ef9bf65b82599b9afaab22cda514aab70c
Michael Phan <sonpython@gmail.com>
2021-05-25 05:10:03 +0700

    silent output log rdisk

--- rdisk.sh
+++ rdisk.sh
@@ -3,9 +3,9 @@
 # auto rename disk base on vendor and serial of the disk
 # usage: . ./rdisk.sh /dev/sda
 
-sudo apt-get install smartmontools -y > /dev/null
-sudo apt-get install ntfsprogs -y > /dev/null
-sudo apt-get install ntfs-3g -y > /dev/null
+sudo apt-get install smartmontools -y &>> /dev/null
+sudo apt-get install ntfsprogs -y &>> /dev/null
+sudo apt-get install ntfs-3g -y &>> /dev/null
 
 HOSTNAME=`hostname`
 DISK_PATH=$1
@@ -23,13 +23,13 @@ MOUNTHOST="/media/$HOSTNAME/"
 MOUNTPOINT="$MOUNTHOST$DISKNAME"
 echo "Identify disk name: $DISKNAME"
 echo "Umount disk $DISK_PATH"
-sudo umount -f -l $DISK_PATH
+sudo umount -f -l $DISK_PATH > /dev/null
 # fix ntfs permission
-sudo ntfsfix $DISK_PATH
-sudo e2label $DISK_PATH "$DISKNAME"
+sudo ntfsfix $DISK_PATH &> /dev/null
+sudo e2label $DISK_PATH "$DISKNAME" > /dev/null
 echo "Remount disk $DISK_PATH with new mount point $MOUNTPOINT"
-sudo mkdir /media/$HOSTNAME > /dev/null
-sudo mkdir /media/$HOSTNAME/$DISKNAME > /dev/null
+sudo mkdir /media/$HOSTNAME &> /dev/null
+sudo mkdir /media/$HOSTNAME/$DISKNAME &> /dev/null
 sudo mount $DISK_PATH $MOUNTPOINT > /dev/null
 sudo chown -R $USER:$USER $MOUNTHOST > /dev/null
 sudo chmod 777 -R $MOUNTHOST > /dev/null
