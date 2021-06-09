1c4d476554063d6565ab02554f7474f9fd7839af
Michael Phan <sonpython@gmail.com>
2021-06-10 06:29:28 +0700

    update add_script v2

--- add_script.sh
+++ add_script.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 # auto clean and update new chia plots directory
-# usage sh add_script.sh /media/farm4
+# change log:
+# NOTICE: v2: do not need to point to /media/farm1. Just point to /media
+# usage sh add_script.sh /media
 
 # remove all plot directory in config.yaml
 sed -i '/\ \ \-\ \/media\/.*$/d' ~/.chia/mainnet/config/config.yaml
@@ -10,14 +12,19 @@ sed -i '/\ \ \-\ \/media\/.*$/d' ~/.chia/mainnet/config/config.yaml
 sed -i 's/\ \ plot_directories\:/\ \ plot_directories\:\n\ \ \- tmpdirectory/g' ~/.chia/mainnet/config/config.yaml
 
 # add directory
+COUNTER_DISK=0
 COUNTER=0
-for d in $1/*; do
+for d in $1/*/*; do
     # chia plots add -d "$d";
     sed -i "s|\ \ plot_directories\:|\ \ plot_directories\:\n\ \ \- $d|g" ~/.chia/mainnet/config/config.yaml
-    COUNTER=$((COUNTER + 1))
-    echo "$COUNTER | Added $d"
+    PLOT_COUNT=$(ls -lah $d/*.plot 2>/dev/null | wc -l)
+    COUNTER=$((COUNTER + PLOT_COUNT))
+    COUNTER_DISK=$((COUNTER_DISK + 1))
+    echo "$COUNTER_DISK | Added $d | Plot: $PLOT_COUNT"
 done
 
 # remove anchor - tmpdirectory
-sed -i '/\ \ \-\ tmpdirectory$/d' ~/.chia/mainnet/config/config.yaml
-echo "added $COUNTER directorie(s)"
+sed -i '/\ \ \-\ tmpdirectory.*?$/d' ~/.chia/mainnet/config/config.yaml
+echo "added $COUNTER_DISK directorie(s)"
+echo "Total disk: $COUNTER_DISK"
+echo "Total plot: $COUNTER"
