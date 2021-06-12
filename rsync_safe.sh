705cc06ec4bd91c12b5283054a7495763841edf6
Michael Phan <sonpython@gmail.com>
2021-06-13 03:40:15 +0700

    add rsync_safe.sh

---
+++ rsync_safe.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+while IFS= read -r line; do
+  IP=$line
+  LOCK_FILE="/tmp/$IP.lock"
+  if [ ! -f "$LOCK_FILE" ]; then
+    trap "rm -f $LOCK_FILE" EXIT
+    touch $LOCK_FILE
+    FREE_SPACE=$(ssh -n -o "StrictHostKeyChecking no" root@$IP "df /dev/sdb | tail -n+2" | awk '{print $4}')
+    PLOT_SIZE=108900000
+    PLOT_NUM=$((FREE_SPACE / PLOT_SIZE))
+    PLOT_NUM=${PLOT_NUM%.*}
+    PLOT_PATH="/home/queue_plot/$IP"
+    echo "$IP | $FREE_SPACE | $PLOT_NUM"
+    mkdir "$PLOT_PATH"
+    mv `ls /home/dest_plot/*.plot | head -$PLOT_NUM` "$PLOT_PATH"
+    rsync --remove-source-files -avzP $PLOT_PATH/*.plot root@$IP:/mnt/plot/
+    break
+  fi
+done <"$1"
